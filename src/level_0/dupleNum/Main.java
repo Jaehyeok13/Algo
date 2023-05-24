ab739f9e12f225f018f136fc1fa73a6b367176aa
jaehyeok seo <star76855@naver.com>
2023-05-24 21:02:43 +0900

    2023.05.24 서재혁 프로그래머스 레벨0 문제 푸쉬

---
+++ src/level_0/dupleNum/Main.java
@@ -0,0 +1,20 @@
+package level_0.dupleNum;
+
+public class Main {
+  public static void  main(String[]  args)throws Exception {
+    Solution solution = new Solution();
+    System.out.println("결과 값" + solution.solution(new int[] {1,1,2,3,4,5}, 1));
+  }
+
+  public static class Solution{
+    // TODO  중복된 숫자 개수
+    public int solution(int[] array, int n) {
+      int answer = 0;
+      for (int i : array) {
+        // answer = i == n ? ++answer : answer;
+        answer += i == n ? 1 : 0;
+      }
+      return answer;
+    }
+  }
+}
