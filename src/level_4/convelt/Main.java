1a8e81d8070cad2ff0c074ecdf9716b35ed718fd
jaehyeok seo <star76855@naver.com>
2023-07-16 15:13:27 +0900

    20230716 알고리즘 문제 푸쉬

---
+++ src/level_4/convelt/Main.java
@@ -0,0 +1,16 @@
+package level_4.convelt;
+
+public class Main {
+  public static void  main(String[]  args)throws Exception {
+    Solution solution = new Solution();
+    System.out.println("결과 값: " + solution.solution(4, 6, 9, new int[]{1, 2, 3, 1, 1, 2}, new int[]{2, 3, 4, 4, 3, 4}, new int[]{3, 3, 3, 3, 1, 2}));
+  }
+
+  public static class Solution{
+    // TODO 컨베이어 벨트 설계
+    public int solution(int n, int m, int k, int[] u, int[] v, int[] l) {
+      int answer = 0;
+      return answer;
+    }
+  }
+}
