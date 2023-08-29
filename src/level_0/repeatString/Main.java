83c5659dc92e6ca7238073159a651400ab33e41b
jaehyeok seo <star76855@naver.com>
2023-08-29 22:45:48 +0900

    20230829 서재혁 알고리즘 level0 반복문자

---
+++ src/level_0/repeatString/Main.java
@@ -0,0 +1,23 @@
+package level_0.repeatString;
+
+import level_0.findModeNum.Main.Solution;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Solution solution = new Solution();
+    System.out.println("결과 값 : " + solution.solution("hello", 3));
+  }
+
+  public static class Solution {
+    // TODO 문자 반복 출력하기
+    public String solution(String my_string, int n) {
+      String answer = "";
+      StringBuilder sb = new StringBuilder();
+      for(int i = 0; i < my_string.length(); i++){
+          sb.append(String.valueOf(my_string.charAt(i)).repeat(n));
+      }
+      
+      return answer = sb.toString();
+    }
+  }
+}
